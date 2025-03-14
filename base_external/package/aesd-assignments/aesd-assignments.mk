5895eb7056db5d2bf38ae99f1f2605df07a6e718
root <root@Ubuntu.myguest.virtualbox.org>
2025-03-14 16:00:56 -0600

    Successfull run

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -6,7 +6,7 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-AESD_ASSIGNMENTS_VERSION = 'cdf816f4f524b9dbd816e37718f8a8431b6b207d'
+AESD_ASSIGNMENTS_VERSION = '132ea3d9d010f24d9040628bd62fd7d4694fb1e5'
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
